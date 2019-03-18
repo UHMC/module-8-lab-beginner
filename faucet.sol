cf4665a084d324924f8156a5072475745fd7c98d
Saxon Knight <knight7@users.noreply.github.com>
2019-03-18 09:11:47 -1000

    Add complete faucet.sol for reference

---
+++ faucet.sol
@@ -0,0 +1,29 @@
+// Version of Solidity compiler this program was written for
+pragma solidity ^0.5.1;
+
+contract Faucet {
+    address owner;
+
+    // Contract constructor: set owner
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    // Contract destructor
+    function destroy() public {
+        require(msg.sender == owner);
+        selfdestruct(msg.sender);
+    }
+
+    // Give out ether to anyone who asks
+    function withdraw(uint withdraw_amount) public {
+        // Limit withdrawal amount
+        require(withdraw_amount <= 0.1 ether);
+
+        // Send the amount to the address that requested it
+        msg.sender.transfer(withdraw_amount);
+    }
+
+    // Accept any incoming amount
+    function () external payable {}
+}
